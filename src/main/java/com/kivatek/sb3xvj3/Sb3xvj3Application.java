c58a5802d5c3e6b8230b578687c3eb0f3c0cd548
kivatek <shinya.hrsk@gmail.com>
2023-01-29 20:09:49 +0900

    プロジェクト初期状態の登録

---
+++ src/main/java/com/kivatek/sb3xvj3/Sb3xvj3Application.java
@@ -0,0 +1,13 @@
+package com.kivatek.sb3xvj3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3xvj3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb3xvj3Application.class, args);
+	}
+
+}
