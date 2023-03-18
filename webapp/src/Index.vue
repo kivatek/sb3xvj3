effe4a1081940666abbf03b372505721bf264452
kivatek <shinya.hrsk@gmail.com>
2023-03-18 23:22:48 +0900

    基本のページ構成を用意

--- webapp/src/Index.vue
+++ webapp/src/Index.vue
@@ -13,22 +13,3 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- webapp/src/index.ts
+++ webapp/src/index.ts
@@ -2,7 +2,4 @@ import { createApp } from 'vue'
 import App from './Index.vue'
 import router from './router/index'
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import './assets/main.css'
-
 createApp(App).use(router).mount("#app");
